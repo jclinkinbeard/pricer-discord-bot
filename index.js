9beff2ebd744f4bf1bb1b59e4a299a74a1054890
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2020-06-07 18:18:55 -0400

    POC commit

--- index.js
+++ index.js
@@ -1,142 +1,44 @@
 require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
-
-const libhoney = require('libhoney')
-const hny = new libhoney({
-  writeKey: process.env.HONEYCOMB_TOKEN || 'asf',
-  dataset: 'discord',
-  serviceName: 'eggo-discord-bot',
-  transmission: 'writer',
-})
-
-const EGGO_BOT_ID = '417792875355701249'
-
-function cleanEmojiDiscriminator(emojiDiscriminator) {
-  var regEx = /[A-Za-z0-9_]+:[0-9]+/
-  var cleaned = regEx.exec(emojiDiscriminator)
-  if (cleaned) return cleaned[0]
-  return emojiDiscriminator
-}
 
 client.once('ready', async () => {
-  console.log('eggo is ready')
-  for (var {channel, message: message_id, reactions} of config) {
-    var channel = await client.channels.cache.get(channel)
-    var message = await channel.messages.fetch(message_id)
-    if (!message) continue
-    for (var {emoji} of reactions) {
-      emoji = cleanEmojiDiscriminator(emoji)
-      var messageReaction = message.reactions.cache.get(emoji)
-      if (!messageReaction) {
-        await message.react(emoji).catch((error) => console.error(error))
-      } else {
-        if (!messageReaction.me) {
-          messageReaction.fetchUsers()
-          await message.react(emoji).catch((error) => console.error(error))
-        }
-      }
-    }
-  }
+  console.log('pricebot is ready!')
 })
 
+const prefix = '!'
 client.on('message', (message) => {
-  let ev = hny.newEvent()
-  ev.add({eventType: 'message', authorId: message.author.id})
-  if (message.author.id === EGGO_BOT_ID) {
-    ev.addField('isBot', true)
-    ev.send()
-    return
-  }
+  if (!message.content.startsWith(prefix)) return
 
-  if (message.content === '!avatar') {
-    // If the message is "what is my avatar"
-    // Send the user's avatar URL
-    message.reply(message.author.displayAvatarURL())
-    ev.addField('avatar', true)
+  const [command] = message.content.substr(1).split(' ')
+
+  switch (command) {
+    case 'ping':
+      message.channel.send('PING PONG!')
+      message.delete()
+      break
+    case 'price':
+      handlePriceRequest(message)
+      break
+    case 'pricenp':
+      handlePriceRequest(message, false)
+      break
+    default:
+      message.channel.send(`Unknown command: ${command}`)
   }
-  ev.send()
 })
 
-function getEmojiDiscriminator(emoji) {
-  if (emoji.id) {
-    return `${emoji.name}:${emoji.id}`
-  } else {
-    return emoji.name
-  }
+function handlePriceRequest(message, notify = true) {
+  const args = message.content.substr('!price'.length + 1)
+  const roles = message.guild.roles.cache
+  const pricerRole = roles.find((r) => r.name === 'Pricer')
+  const pitRole = roles.find((r) => r.name === 'Pricer in Training')
+  let msg = ''
+  msg += `Price request! <@${message.author.id}> wants to know `
+  msg += `the price of **${args}** for Nintendo Switch.`
+  if (notify) msg += `<@&${pricerRole.id}> <@&${pitRole.id}>`
+  message.channel.send(msg)
 }
 
-// only works on cached messages (messages received while client open)
-client.on('messageReactionAdd', async (messageReaction, user) => {
-  if (user == client.user) return
-  var member = await messageReaction.message.guild.members.cache.get(user.id)
-  var emojiDiscriminator = getEmojiDiscriminator(messageReaction.emoji)
-  for (var {channel, reactions, disjoint} of config) {
-    if (channel != messageReaction.message.channel.id) continue
-    var rolesNew = []
-    for (var role of member.roles.cache.keys()) {
-      rolesNew.push(role)
-    }
-    var rolesAllowList = []
-    var rolesBlockList = []
-    for (var {emoji, roles} of reactions) {
-      if (emojiDiscriminator == emoji) {
-        rolesAllowList.push.apply(rolesAllowList, roles) //Prototyping the push function, might be buggy
-      }
-      rolesBlockList.push.apply(rolesBlockList, roles)
-    }
-    //Check to see if roles are handled mutually eclusive
-    if (disjoint) {
-      rolesNew = rolesNew.filter(
-        (role) =>
-          //Remove role if found on watchlist
-          !rolesBlockList.includes(role),
-      )
-    }
-    rolesNew.push.apply(rolesNew, rolesAllowList)
-    //Make sure none of the roles on the "add" list get removed again
-    await member.roles.set(rolesNew).catch((error) => console.error(error))
-    if (disjoint)
-      await messageReaction.remove(user).catch((error) => console.error(error))
-  }
-})
-
-client.on('messageReactionRemove', async (messageReaction, user) => {
-  //Bot should not react to its own reactions.
-  if (user == client.user) return
-  var member = await messageReaction.message.guild.members.cache.get(user.id)
-  var emojiDiscriminator = getEmojiDiscriminator(messageReaction.emoji)
-  ;(async () => {
-    for (var {disjoint, channel, reactions} of config) {
-      //Make sure we're not in "disjoint" mode
-      if (disjoint) continue
-      if (channel != messageReaction.message.channel.id) continue
-      var rolesToKeep = []
-      var rolesToRemove = []
-      for (var {emoji, roles} of reactions) {
-        if (emojiDiscriminator == emoji) {
-          //Add to removal list
-          rolesToRemove.push.apply(rolesToRemove, roles)
-        } else {
-          //List of all other roles that should be kept
-          rolesToKeep.push.apply(rolesToKeep, roles)
-        }
-      }
-      rolesToRemove.filter(
-        (role) =>
-          //Make sure role that is about to be removed is not part of another emoji
-          !rolesToKeep.includes(role) &&
-          //Make sure member actually has role
-          member.roles.cache.get(role),
-      )
-      await member.roles
-        .remove(rolesToRemove)
-        .catch((error) => console.error(error))
-    }
-  })()
-})
-
 // https://discordapp.com/channels/365559264850477066/715004775300726825/715013654575054898
-hny.sendNow({booting: true})
 client.login(process.env.BOT_TOKEN)
