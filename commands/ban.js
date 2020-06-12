e7e66e68dcd189e4a0f188628e26be890b2a6b22
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2020-06-11 22:00:02 -0400

    Ban restriction

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,17 @@
-const { COMMANDS } = require('../constants')
+const { ROLES, COMMANDS } = require('../constants')
+const { findRoleByName } = require('../utils')
+
+const adminRoleNames = [ROLES.ADMINISTRATOR, ROLES.OWNER]
 
 module.exports = function (message, command, request) {
+  const isBannerAdmin = message.member.roles.cache.some((r) => {
+    return adminRoleNames.includes(r.name)
+  })
+
+  if (!isBannerAdmin) {
+    return 'You tried to do a bad thing and you should feel bad.'
+  }
+
   let msg = ''
   if (command == COMMANDS.BAN) {
     const toBan = message.mentions.members.first().id
