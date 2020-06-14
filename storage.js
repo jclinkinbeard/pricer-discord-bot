0180bc80aaac418d9d1f8fae50c3032167fdaf9d
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2020-06-13 20:10:33 -0400

    Fix the storage namespace

--- storage.js
+++ storage.js
@@ -1,10 +1,10 @@
 const Keyv = require('keyv')
 
 const { LOCAL, MONGODB_URI } = process.env
-const env = LOCAL ? 'local' : ''
+const env = LOCAL ? 'local:' : ''
 
-exports.fcStorage = new Keyv(MONGODB_URI, { namespace: `${env}:fc` })
+exports.fcStorage = new Keyv(MONGODB_URI, { namespace: `${env}fc` })
 exports.fcStorage.on('error', (err) => {
   console.error('Keyv connection error:', err)
 })
-exports.repStorage = new Keyv(MONGODB_URI, { namespace: `${env}:rep` })
+exports.repStorage = new Keyv(MONGODB_URI, { namespace: `${env}rep` })
